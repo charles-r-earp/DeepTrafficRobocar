21d97d44bbabbb8baa148f81f45a60b751c2c7f6
charles-r-earp <charles.r.earp@gmail.com>
2018-01-16 21:32:42 -0800

    Update net.js

--- net.js
+++ net.js
@@ -3,13 +3,13 @@
 
 // a few things don't have var in front of them - they update already existing variables the game needs
 lanesSide = 1;
-patchesAhead = 10;
-patchesBehind = 4;
+patchesAhead = 2;
+patchesBehind = 1;
 trainIterations = 1000;
 
 var num_inputs = (lanesSide * 2 + 1) * (patchesAhead + patchesBehind);
 var num_actions = 5;
-var temporal_window = 10;
+var temporal_window = 1;
 var network_size = num_inputs * temporal_window + num_actions * temporal_window + num_inputs;
 
 var layer_defs = [];
@@ -22,6 +22,13 @@ layer_defs.push({
 layer_defs.push({
     type: 'fc',
     num_neurons: num_inputs,
+    activation: 'relu'
+});
+layer_defs.push({
+    type: 'conv',
+    sx: 1,
+    sy: 3,
+    stride: 1,
     activation: 'relu'
 });
 layer_defs.push({
@@ -43,7 +50,7 @@ opt.start_learn_threshold = 500;
 opt.gamma = 0.8;
 opt.learning_steps_total = 10000;
 opt.learning_steps_burnin = 1000;
-opt.epsilon_min = 0.08;
+opt.epsilon_min = 0.1;
 opt.epsilon_test_time = 0.0;
 opt.layer_defs = layer_defs;
 opt.tdtrainer_options = tdtrainer_options;
