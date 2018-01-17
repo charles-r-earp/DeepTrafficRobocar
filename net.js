b375f993fa8d39c92a89c4e9346d2113373f0823
charles-r-earp <charles.r.earp@gmail.com>
2018-01-16 23:43:33 -0800

    Update net.js

--- net.js
+++ net.js
@@ -3,32 +3,31 @@
 
 // a few things don't have var in front of them - they update already existing variables the game needs
 lanesSide = 3;
-patchesAhead = 12;
-patchesBehind = 8;
-trainIterations = 5000;
+patchesAhead = 50;
+patchesBehind = 10;
+trainIterations = 300000;
 
 var num_inputs = (lanesSide * 2 + 1) * (patchesAhead + patchesBehind);
 var num_actions = 5;
 var temporal_window = 0;
-
-var input_size = num_inputs * (temporal_window+1) + num_actions * temporal_window;
+var network_size = num_inputs * temporal_window + num_actions * temporal_window + num_inputs;
 
 var layer_defs = [];
 layer_defs.push({
     type: 'input',
     out_sx: 1,
     out_sy: 1,
-    out_depth: input_size
+    out_depth: network_size
 });
 layer_defs.push({
     type: 'fc',
-    num_neurons: input_size / 2,
-    activation: 'relu'
+    num_neurons: 300,
+    activation: 'tanh'
 });
 layer_defs.push({
     type: 'fc',
-    num_neurons: input_size / 4,
-    activation: 'relu'
+    num_neurons: 400,
+    activation: 'tanh'
 });
 layer_defs.push({
     type: 'regression',
@@ -37,21 +36,21 @@ layer_defs.push({
 
 var tdtrainer_options = {
     learning_rate: 0.001,
-    momentum: 0.5,
-    batch_size: 64,
-    l2_decay: 0.05
+    momentum: 0.0,
+    batch_size: 128,
+    l2_decay: 0.01
 };
 
 var opt = {};
 opt.temporal_window = temporal_window;
-opt.experience_size = 3000;
-opt.start_learn_threshold = 500;
-opt.gamma = 0.8;
+opt.experience_size = 40000;
+opt.start_learn_threshold = 10000;
+opt.gamma = 0.97;
 opt.learning_steps_total = 10000;
 opt.learning_steps_burnin = 1000;
-opt.epsilon_min = 0.3;
-opt.epsilon_test_time = 0.1;
-opt.layer_defs = layer_defs;
+opt.epsilon_min = 0.0;
+opt.epsilon_test_time = 0.0;
+opt.layer_defas = layer_defs;
 opt.tdtrainer_options = tdtrainer_options;
 
 brain = new deepqlearn.Brain(num_inputs, num_actions, opt);
@@ -66,4 +65,4 @@ learn = function (state, lastReward) {
     return action;
 }
 
-//]]>    
+//]]>
