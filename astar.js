bb14321fedfadf64f5d14c31502b467a52dadbd7
charles-r-earp <charles.r.earp@gmail.com>
2019-01-31 21:49:39 -0800

    Update astar.js

--- astar.js
+++ astar.js
@@ -23,11 +23,88 @@ remap_occupied = function(state) {
     return state;
 }
 
+//https://stackoverflow.com/questions/42919469/efficient-way-to-implement-priority-queue-in-javascript
+class PriorityQueue {
+  constructor(comparator = (a, b) => a > b) {
+    this._heap = [];
+    this._comparator = comparator;
+  }
+  size() {
+    return this._heap.length;
+  }
+  isEmpty() {
+    return this.size() == 0;
+  }
+  peek() {
+    return this._heap[top];
+  }
+  push(...values) {
+    values.forEach(value => {
+      this._heap.push(value);
+      this._siftUp();
+    });
+    return this.size();
+  }
+  pop() {
+    const poppedValue = this.peek();
+    const bottom = this.size() - 1;
+    if (bottom > top) {
+      this._swap(top, bottom);
+    }
+    this._heap.pop();
+    this._siftDown();
+    return poppedValue;
+  }
+  replace(value) {
+    const replacedValue = this.peek();
+    this._heap[top] = value;
+    this._siftDown();
+    return replacedValue;
+  }
+  _greater(i, j) {
+    return this._comparator(this._heap[i], this._heap[j]);
+  }
+  _swap(i, j) {
+    [this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]];
+  }
+  _siftUp() {
+    let node = this.size() - 1;
+    while (node > top && this._greater(node, parent(node))) {
+      this._swap(node, parent(node));
+      node = parent(node);
+    }
+  }
+  _siftDown() {
+    let node = top;
+    while (
+      (left(node) < this.size() && this._greater(left(node), node)) ||
+      (right(node) < this.size() && this._greater(right(node), node))
+    ) {
+      let maxChild = (right(node) < this.size() && this._greater(right(node), left(node))) ? right(node) : left(node);
+      this._swap(node, maxChild);
+      node = maxChild;
+    }
+  }
+}
+//
+
+class Node {
+    constructor(pos) {
+        this.actions = [];
+        this.path = [pos];
+        this.heuristic = pos[1];
+        this.cost = this.heuristic;
+    }
+    next() {
+        pos = this.path[length - 1];
+        
+
 astar_search = function(map, start) {
   occupied = new convnetjs.Vol(width, height, 1);
   occupied.w = state;
   visited = new convnetjs.Vol(width, height, 1, 0);
-  queue = 
+  queue = new PriorityQueue((a, b) => a.cost < b.cost);
+  queue.push(
 
 learn = function (state, lastReward) {
     map = new convnetjs.Vol(width, height, 1, 0);
