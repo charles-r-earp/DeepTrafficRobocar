cb0eedde1eda1437eab14ffeda540bad6065e8ea
charles-r-earp <charles.r.earp@gmail.com>
2019-02-05 13:23:27 -0800

    Update net.js

--- net.js
+++ net.js
@@ -2,9 +2,9 @@
 //<![CDATA[
 
 // a few things don't have var in front of them - they update already existing variables the game needs
-lanesSide = 8;
+lanesSide = 6;
 patchesAhead = 52;
-patchesBehind = 18;
+patchesBehind = 20;
 trainIterations = 40000;
 
 // the number of other autonomous vehicles controlled by your network
@@ -16,6 +16,81 @@ var num_inputs = (lanesSide * 2 + 1) * (patchesAhead + patchesBehind);
 var num_actions = 5;
 var temporal_window = 0;
 //var network_size = num_inputs * temporal_window + num_actions * temporal_window + num_inputs;
+
+class Node {
+    constructor(pos, action, reward, depth = 0) {
+        this.pos = pos;
+        this.action = action;
+        this.reward = reward;
+        this.depth = depth;
+    }
+    next(pos, reward, action) {
+        return new Node(pos, this.depth ? this.action : action, reward, this.depth + 1);
+    }
+}
+
+class Map {
+    constructor(shape, data = 0) {
+        if (data ) {
+            length = shape[0] * shape[1];
+        }
+        
+astar_search = function(map, start) {
+  var node = new Node(start);
+  var queue = [node];
+  while (queue.length) {
+      node = queue.pop();
+      if (node.pos[1] == 3) {
+          return node.action;
+      }
+      if (node.pos[1] > 0) {
+          var up = [node.pos[0], node.pos[1]-1];
+          var clear = 1;
+          for (i = -5; i<0; ++i) {
+            if (map.get(up[0], up[1]+i, 0)) {
+              clear = 0;
+              break;
+            }
+          }
+          if (clear) {
+            map.set(up[0], up[1], 0, 1);
+            queue.push(node.next(up, 1));
+          }
+      }
+      if (node.pos[0] > 0) {
+          var left = [node.pos[0]-1, node.pos[1]];
+          var clear = 1;
+          for (i = -6; i<4; ++i) {
+              if (map.get(left[0], left[1]+i, 0)) {
+                  clear = 0;
+                  break;
+              }
+          }
+          if (clear) {
+              map.set(left[0], left[1], 0, 1);
+              queue.push(node.next(left, 3));
+          }
+      }
+      if (node.pos[0] < map.sx) {
+          var right = [node.pos[0]+1, node.pos[1]];
+          var clear = 1;
+          for (i = -6; i<4; ++i) {
+              if (map.get(right[0], right[1]+i, 0)) {
+                  clear = 0;
+                  break;
+              }
+          }
+          if (clear) {
+              map.set(right[0], right[1], 0, 1);
+              queue.push(node.next(right, 4));
+          }
+      }
+      queue.sort(function (a,b){ return a.cost != b.cost ? a.cost < b.cost : a.action < b.action; });
+  }
+  return node.action;
+}
+
+class Map
 
 var layer_defs = [];
 layer_defs.push({
@@ -39,10 +114,10 @@ var tdtrainer_options = {
 var opt = {};
 opt.temporal_window = temporal_window;
 opt.experience_size = 3000;
-opt.start_learn_threshold = 500;
-opt.gamma = 0.1;
+opt.start_learn_threshold = 0;
+opt.gamma = 0.98;
 opt.learning_steps_total = trainIterations;
-opt.learning_steps_burnin = 1000;
+opt.learning_steps_burnin = 0;
 opt.epsilon_min = 0.0;
 opt.epsilon_test_time = 0.0;
 opt.layer_defs = layer_defs;
@@ -50,17 +125,17 @@ opt.tdtrainer_options = tdtrainer_options;
 
 brain = new deepqlearn.Brain(num_inputs, num_actions, opt);
 
-occupied = function(state) {
-    for (i = 0; i<num_inputs; ++i) {
-        state[i] = state[i] == 1 ? 1 : -1;
-    }
-    return state;
-}
-
 learn = function (state, lastReward) {
+    var map = new convnetjs.Vol(width, height, 1, 0);
+    for (i = 0; i<state.length; ++i) {
+        map.w[i] = state[i] == 1 ? 0 : 1;
+    }
+    for (i = 0; i<4; ++i) {
+        map.set(lanesSide, patchesAhead-4+1, 1, 0);
+    }
     brain.backward(lastReward);
-    state = occupied(state);
-    var action = brain.forward(state);
+    brain.forward(map.w);
+    action = astar_search(map, [lanesSide, patchesAhead]);
 
     draw_net();
     draw_stats();
