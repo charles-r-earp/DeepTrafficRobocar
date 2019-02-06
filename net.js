75727074dde8f05d862653a9f1919ff03c4b79c7
charles-r-earp <charles.r.earp@gmail.com>
2019-02-06 11:02:07 -0800

    Update net.js

--- net.js
+++ net.js
@@ -17,7 +17,7 @@ var num_actions = 5;
 var temporal_window = 0;
 //var network_size = num_inputs * temporal_window + num_actions * temporal_window + num_inputs;
 
-/*class Node {
+class Node {
     constructor(pos, action, reward, depth = 0) {
         this.pos = pos;
         this.action = action;
@@ -27,24 +27,24 @@ var temporal_window = 0;
     next(pos, reward, action) {
         return new Node(pos, this.depth ? this.action : action, reward, this.depth + 1);
     }
-}*/
+}
 
-/*
-class Map2D {
+
+class Map {
     constructor(shape, data) {
-        //this.shape = shape;
+        this.shape = shape;
         if (data.constructor == Array) {
             this.data = data;
         }
         else {
             this.data = [];
-            length = shape[0] * shape[1];
-            for (i = 0; i < length; ++i) {
+            var length = shape[0] * shape[1];
+            for (var i = 0; i < length; ++i) {
                 this.data.push(data);
             }
         }
     }
-    /*index(pos) {
+    index(pos) {
         return pos[0] * this.shape[1] + pos[1];
     }
     get(pos) {
@@ -56,8 +56,8 @@ class Map2D {
     shift(delta, fill = 0) {
         var map = new Map(this.shape, fill);
         var from, to;
-        for (i = 0; i < this.shape[0]; ++i) {
-            for (j = 0; j < this.shape[1]; ++j) {
+        for (var i = 0; i < this.shape[0]; ++i) {
+            for (var j = 0; j < this.shape[1]; ++j) {
                 from = [i, j] 
                 to = [i+delta[0], j+delta[1]];
                 if (to[0] >= 0 && to[0] < this.shape[0] && to[1] >= 0 && to[1] < this.shape[1]) {
@@ -68,7 +68,7 @@ class Map2D {
         return map;
     }
 }
-*/
+
 var layer_defs = [];
 layer_defs.push({
     type: 'input',
@@ -101,7 +101,7 @@ opt.layer_defs = layer_defs;
 opt.tdtrainer_options = tdtrainer_options;
 
 brain = new deepqlearn.Brain(num_inputs, num_actions, opt);
-/*
+
 astar_search = function(speeds, start) {
   var visited = new Map(speeds.shape, 0);
   var node = new Node(start);
@@ -112,13 +112,11 @@ astar_search = function(speeds, start) {
           continue;
       }
       visited.set(node.pos, 1);
-      continue;
-      
       // up
       if (node.pos[1] > 0) {
           var up = [node.pos[0], node.pos[1]-1];
           var clear = 1;
-          for (i = -5; i<0; ++i) {
+          for (var i = -5; i<0; ++i) {
             if (speeds.get(up) == 1) {
               clear = 0;
               break;
@@ -134,7 +132,7 @@ astar_search = function(speeds, start) {
       if (node.pos[0] > 0) {
           var left = [node.pos[0]-1, node.pos[1]];
           var clear = 1;
-          for (i = -6; i<4; ++i) {
+          for (var i = -6; i<4; ++i) {
               if (speeds.get(left) == 1) {
                   clear = 0;
                   break;
@@ -147,10 +145,10 @@ astar_search = function(speeds, start) {
             queue.push(node.next(left, action, reward));
           }
       }
-      if (node.pos[0] < map.sx) {
+      if (node.pos[0] < visited.shape[0]) {
           var right = [node.pos[0]+1, node.pos[1]];
           var clear = 1;
-          for (i = -6; i<4; ++i) {
+          for (var i = -6; i<4; ++i) {
               if (speeds.get(right) == 1) {
                   clear = 0;
                   break;
@@ -163,28 +161,25 @@ astar_search = function(speeds, start) {
             queue.push(node.next(right, action, reward));
           }
       }
-      queue.sort(function (a,b){ return a.reward > b.reward; });
+      queue.sort(function (a,b){ return a.reward > b.reward; });   
   }
   return node.action;
-  return 0;
-}*/
+}
 
-//var lastState = 0;
+var lastState = 0;
 
 learn = function (state, lastReward) {
-    return 0;
-    /*if (lastState) {
+    if (lastState) {
         brain.forward(lastState);
         brain.backward(lastReward);
     }
-    lastState = state;*/
-    //var speeds = [];//[width, height], state);
-    //var action = 0;// astar_search(speeds, [lanesSide, patchesAhead]);
-   // var action = 0;
-    //draw_net();
-    //draw_stats();
+    lastState = state;
+    var speeds = new Map([width, height], state);
+    var action = astar_search(speeds, [lanesSide, patchesAhead]);
+    draw_net();
+    draw_stats();
 
-    return action;
+    return action; 
 }
 
 //]]>
